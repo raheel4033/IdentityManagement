b716d0f320c40907993ba3909a8107346efcb6d1
raheel4033 <raheelh717@hotmail.com>
2022-09-12 15:10:48 -0500

    Added : AccountController and Password Validations

---
+++ IdentityManager/IdentityManager/Controller/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using IdentityManager.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager; 
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return Ok();
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return Ok(); 
+        }
+
+    }
+}
--- IdentityManager/IdentityManager/Program.cs
+++ IdentityManager/IdentityManager/Program.cs
@@ -5,7 +5,13 @@ using Microsoft.EntityFrameworkCore.SqlServer;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+      {
+          options.Password.RequiredLength = 10;
+          options.Password.RequiredUniqueChars = 3;
+          options.Password.RequireNonAlphanumeric = false;
+      }
+      )
     .AddEntityFrameworkStores<AppDbContext>();
 app.MapGet("/", () => "Hello World!");
 
